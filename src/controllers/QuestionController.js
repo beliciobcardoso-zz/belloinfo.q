63e6370f53d9a691ff70e694d31894639e42cbe4
Belicio Batista Cardoso <belloinfo@gmail.com>
2021-06-25 18:24:24 -0300

    include de paginas

--- src/controllers/QuestionController.js
+++ src/controllers/QuestionController.js
@@ -4,7 +4,7 @@ module.exports = {
         const roomId = request.params.room
         const questionId = request.params.question
         const action = request.params.action
-        const password = request.params.password
+        const password = request.body.password
 
         console.log(`roomId = ${roomId}, questionId = ${questionId}, action = ${action}, password = ${password}`)
     }
--- src/route.js
+++ src/route.js
@@ -5,8 +5,8 @@ const express = require('express');
 
 const route = express.Router();
 
-route.get('/', (request, respond) => respond.render('index'));
-route.get('/create_pass', (request, respond) => respond.render('create_pass'));
+route.get('/', (request, respond) => respond.render('index', { page: 'enter_room' }));
+route.get('/create_pass', (request, respond) => respond.render('index', { page: 'create_pass' }));
 route.get('/room', (request, respond) => respond.render('room'));
 
 route.post('/room/:room/:question/:action', QuestionControllers.index);
