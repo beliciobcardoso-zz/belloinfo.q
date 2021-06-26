6f37c8e0d6bdf1a173776aa06c2eb9ba3ef8b3aa
Belicio Batista Cardoso <belloinfo@gmail.com>
2021-06-25 22:27:37 -0300

    configurado o insert da room

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -1,7 +1,21 @@
 'use strict'
+const dataBase = require('../db/config');
+
 module.exports = {
-    create(request, respond) {
-        let roomId = 271177;
+    async create(request, respond) {
+        const db = await dataBase();
+        const pass = request.body.password;
+
+        let roomId = "";
+
+        for (let index = 0; index < 6; index++) {
+            roomId += Math.floor(Math.random() * 10).toString();
+        }
+        roomId = parseInt(roomId);
+        await db.run(`INSERT INTO rooms (id, pass) VALUES (${roomId}, ${pass}) `);
+
+        await db.close();
+
         respond.redirect(`/room/${roomId}`);
     }
 }
